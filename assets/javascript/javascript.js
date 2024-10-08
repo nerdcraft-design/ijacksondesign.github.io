d9ab20274e52c442cc71bffb3fc5e0095d106ec8
Ian Jackson <75548200+nerdcraft-design@users.noreply.github.com>
2024-10-08 09:22:56 -0600

    added conditional to provide portfolio links

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -22,6 +22,7 @@ const projects = [
         thumbnailAlt: 'AI for Demand Gen Marketers logo',
         briefParagraph1: 'AI for Demand Gen Marketers is a 10-week webinar series put on by MarketingProfs to help B2B marketers learn how to use AI to improve their demand generation efforts. ',
         briefParagraph2: 'The logo was created by combining a robot with magnets to represent the idea of demand generation. The electricity bolts from the logo were then used to create a background pattern and as decorative design elements. To promote the event, I also designed and coded the event microsite and created all the marketing assets including email and social media graphics.',
+        projectLink: 'https://www.marketingprofs.com/events/ai-for-demand-gen-marketers/home/sa/',
         firstImg: 'aidg-1.jpg',
         firstImgAlt: 'Screenshots of the AI for Demand Gen Marketers website displayed across several devices',
         secondImg: 'aidg-2.jpg',
@@ -38,6 +39,7 @@ const projects = [
         thumbnailAlt: 'Marketing Writing Bootcamp logo',
         briefParagraph1: 'In 2020, MarketingProfs put on a webinar series called Marketing Writing Bootcamp which came to be their most successful program ever. In 2024 they decided to bring the event back and I was tasked to create the event branding and promotional assets.',
         briefParagraph2: 'A badge logo was created along with a background pattern. The colors were made to mimic the classic No.2 pencil and a scribble graphic was made as a design element to create accents and callouts. To promote the event, I also designed and coded the event microsite and created all the marketing assets including email and social media graphics.',
+        projectLink: 'https://www.marketingprofs.com/events/mwb24/home/sa/',
         firstImg: 'mwb-1.jpg',
         firstImgAlt: 'Screenshots of the Marketing Writing Bootcamp website displayed across several devices',
         secondImg: 'mwb-2.jpg',
@@ -107,7 +109,86 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const newCol = document.createElement("div");
         newCol.classList.add('col');
-        newCol.innerHTML += `<div class="card text-bg-dark mb-3 mb-lg-5 grid-item">
+        if (project.projectLink) {
+            newCol.innerHTML += `<div class="card text-bg-dark mb-3 mb-lg-5 grid-item">
+                            <img src="./assets/images/portfolio/${project.thumbnail}"
+                                class="card-img" alt="${project.mainthumbnailAlt}">
+                            <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
+                                <h3 class="card-title">${project.name}</h3>
+                                <p class="card-text fs-5">${project.category}</p>
+                                <button type="button" class="btn btn-secondary" data-bs-toggle="modal"
+                                    data-bs-target="#modal${counter}">
+                                    See More >
+                                </button>
+                            </div>
+                        </div>
+                        <div class="modal fade" id="modal${counter}" tabindex="-1" aria-labelledby="modal${counter}Label"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-fullscreen modal-dialog-centered modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body d-flex align-items-center">
+                                        <div class="container-xxl">
+                                            <div class="row justify-content-between align-items-center">
+                                                <div class="col-12 col-mg-6 col-lg-4 col-xxl-4">
+                                                    <h1 class="modal-title fs-1" id="modal${counter}Label">
+                                                    ${project.name}
+                                                    </h1>
+                                                    <p>
+                                                    ${project.briefParagraph1}
+                                                    </p>
+                                                    <p>
+                                                    ${project.briefParagraph2}
+                                                    </p>
+                                                    <a href="${project.projectLink}" target="_blank">Link to ${project.name}</a>
+                                                </div>
+                                                <div class="col-12 col-mg-6 col-lg-7 col-xxl-8">
+                                                    <div id="projectCarousel${counter}" class="carousel slide" data-bs-ride="carousel">
+                                                        <div class="carousel-indicators">
+                                                            <button type="button" data-bs-target="#projectCarousel${counter}" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                            <button type="button" data-bs-target="#projectCarousel${counter}" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                            <button type="button" data-bs-target="#projectCarousel${counter}" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                                            <button type="button" data-bs-target="#projectCarousel${counter}" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                                                        </div>
+                                                        <div class="carousel-inner">
+                                                            <div class="carousel-item active">
+                                                                <img src="./assets/images/portfolio/${project.firstImg}" class="d-block w-100" alt="${project.firstImgAlt}">
+                                                            </div>
+                                                            <div class="carousel-item">
+                                                                <img src="./assets/images/portfolio/${project.secondImg}" class="d-block w-100" alt="${project.secondImgAlt}">
+                                                            </div>
+                                                            <div class="carousel-item">
+                                                                <img src="./assets/images/portfolio/${project.thirdImg}" class="d-block w-100" alt="${project.thirdImgAlt}">
+                                                            </div>
+                                                            <div class="carousel-item">
+                                                                <img src="./assets/images/portfolio/${project.fourthImg}" class="d-block w-100" alt="${project.fourthImgAlt}">
+                                                            </div>
+                                                        </div>
+                                                        <button class="carousel-control-prev" type="button" data-bs-target="#projectCarousel${counter}" data-bs-slide="prev">
+                                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                            <span class="visually-hidden">Previous</span>
+                                                        </button>
+                                                        <button class="carousel-control-next" type="button" data-bs-target="#projectCarousel${counter}" data-bs-slide="next">
+                                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                            <span class="visually-hidden">Next</span>
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-lg btn-outline-primary"
+                                            data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+        } else {
+            newCol.innerHTML += `<div class="card text-bg-dark mb-3 mb-lg-5 grid-item">
                             <img src="./assets/images/portfolio/${project.thumbnail}"
                                 class="card-img" alt="${project.mainthumbnailAlt}">
                             <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
@@ -183,6 +264,8 @@ document.addEventListener("DOMContentLoaded", function () {
                                 </div>
                             </div>
                         </div>`;
+        }
+        
         portfolioEl.appendChild(newCol);
         counter++;
         console.log(counter);
