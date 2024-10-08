bc71838ec547623486c102472eac5a5466c9bb8f
Ian Jackson <75548200+nerdcraft-design@users.noreply.github.com>
2024-10-08 09:23:53 -0600

    removed commented out text

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -271,18 +271,3 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log(counter);
     });
 });
-
-
-// const buttonEL = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-// function addGoal() {
-//     const enteredValue = inputEl.value;
-//     const listItemEl = document.createElement('li');
-//     listItemEl.textContent = enteredValue;
-//     listEl.appendChild(listItemEl);
-//     inputEl.value = '';
-// }
-
-// buttonEL.addEventListener('click', addGoal);
