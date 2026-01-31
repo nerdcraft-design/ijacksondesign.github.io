f996fb99abd093cd2577757239f1ed4b3cda8c8e
Ian Jackson <ijacksondesign@gmail.com>
2026-01-30 17:50:40 -0700

    added layouteffect to project page

--- src/pages/ProjectItem/index.jsx
+++ src/pages/ProjectItem/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLayoutEffect } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -22,6 +22,10 @@ export default function ProjectItem() {
   if (project === null) {
     return <Spinner animation="border" variant="primary" />;
   }
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
   return (
     <>
